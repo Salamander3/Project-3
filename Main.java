8263346719e0f7d2df557c6a6b4e891d2291cee2
Salamander3 <benjamin.sentirmay@patelhs.org>
2020-11-04 17:28:02 +0000

    Added scanner object and Student name gatherer.

--- Main.java
+++ Main.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
+    //Adds the scanner
+    Scanner scan = new Scanner(System.in);
+
     //Array of seat number
     int[] seatNumber = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
     //Array of student names
     String[] studentNames = new String[15];
     
+    //Main loop for asking for student names
+    for(int index = 0; index < studentNames.length; index++)
+    {
+      System.out.println("Enter Student Name");
+      studentNames[index] = scan.nextLine();
+    }
   }
 }
