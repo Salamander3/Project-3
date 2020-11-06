b64e14d2d40290747773f011b43e521e87f40933
Salamander3 <benjamin.sentirmay@patelhs.org>
2020-11-06 14:57:11 +0000

    Converted the last loop to a while loop from a for loop.

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
+
     //Adds the scanner
     Scanner scan = new Scanner(System.in);
 
     //Array of seat number
     int[] seatNumber = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    
     //Array of student names
     String[] studentNames = new String[15];
     
@@ -14,19 +16,21 @@ class Main {
     for(int index = 0; index < studentNames.length; index++)
     {
       System.out.println("Enter Student Name");
-      {
-        studentNames[index] = scan.nextLine();
-        System.out.println();
-      }
+      studentNames[index] = scan.nextLine();
+      System.out.println();
     }
 
     //Spacing to make it look pretty
     System.out.println();
 
+    // Declaers and sets an index
+    int index = 0;
+    
     //Loop that prints out students seating locations
-    for(int index = 0; index < studentNames.length;index++)
+    while(index < studentNames.length)
     {
-      System.out.println(studentNames[index] + " stits at desk " + seatNumber[index]);
+      System.out.println(studentNames[index] + " sits at desk " + seatNumber[index]);
+      index++;
     }
   }
 }
